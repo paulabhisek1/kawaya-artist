74337e793a3fe120b07ac28f2956916a55139cfa
tuhin koley <tuhinkoley2012@gmail.com>
2021-02-28 21:45:02 +0530

    update

--- src/app_backup/shared/custom-validators/nospacesvalidator.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, FormGroup } from '@angular/forms';
-
-export function noSpace(control: AbstractControl) {
-	if (control.value && control.value.toString().trim() == '') {
-		return { noSpace: true };
-	}
-	return null;
-}
