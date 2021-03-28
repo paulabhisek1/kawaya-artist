6ed9e6e8c697c5611025b8310cdb972b0e1b3875
Suman Rana <suman.r@unifiedinfotech.net>
2021-03-28 23:32:47 +0530

    Up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'http://45.90.108.110:3000/artist/'
+  apiURL: 'http://45.90.108.110:3000/artist/',
+  imageURL: 'http://45.90.108.110:3000'
 };
